7b71355da32e803a886b0939b20159977b05dca2
Luca1882 <luca.notarfranco@gmail.com>
2025-01-30 23:59:51 +0100

    Aggiunto costruttore più genero codice  random

--- src/Prodotto.java
+++ src/Prodotto.java
@@ -1,5 +1,27 @@
+import java.util.Random;
+
+
 public class Prodotto {
     private int codice;
     private String nome;
+    private String descrizione;
+    private double prezzo;
+    private double iva;
 
+    //COSTRUTTORE
+    public Prodotto(String nome, String descrizione, double prezzo, double iva){
+        this.codice = generaCodice();
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
+    //GENERO IL CODICE TRAMITE RANDOM (1-100000)
+    private int generaCodice(){
+        Random codRandom = new Random();
+        return codRandom.nextInt(100000);
+    }
+
+    //
 }
